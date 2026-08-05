51896616ac3514dc80f873de2ee3951c563f33f7
Mahaveer Ram P <mahaveer.p@azimpremjifoundation.org>
2026-08-05 22:40:44 +0530

    Add missing applicant_master.json schema, remove leftover zayam_datas files

--- ms_calendar/ms_calendar/doctype/zayam_datas/zayam_datas.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Augustin Moses and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Zayam Datas", {
-// 	refresh(frm) {
-
-// 	},
-// });
