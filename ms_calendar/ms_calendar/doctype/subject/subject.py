407623c26b4186ce07f86244a09ad4933900e4c9
Nishanth Clinton A <nishanth.a@azimpremjifoundation.org>
2026-09-02 12:46:28 +0530

    feat: add Subject Master DocType

---
+++ ms_calendar/ms_calendar/doctype/subject/subject.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, Augustin Moses and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class Subject(Document):
+	pass
